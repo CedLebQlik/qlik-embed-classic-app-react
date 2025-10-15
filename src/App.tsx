e3c2480e3d3c8549403e71f9eb3e4ee3778557b1
CedLebQlik <166514548+CedLebQlik@users.noreply.github.com>
2025-10-15 13:54:41 +0200

    Add env variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import type { HostConfig } from "@qlik/api/auth";
 import { QlikEmbed, QlikEmbedConfig } from "@qlik/embed-react";
 import './App.css'
 
-const appId = '1c24f07d-039e-4371-980d-27717f01542c';
-const sheetId = '05743d07-2e17-4c5b-8f81-625c4ee0931b';
+const appId = import.meta.env.VITE_QLIK_APPID;
+const sheetId = import.meta.env.VITE_QLIK_SHEETID;
 
 const hostConfig: HostConfig = {
-  host: "https://cedleb.eu.qlikcloud.com",
-  clientId: "94b6ceff3c82c917ad1351848327d9ae",
-  redirectUri: "http://localhost:5173/oauth-callback.html",
+  host: import.meta.env.VITE_QLIK_TENANT_URL,
+  clientId: import.meta.env.VITE_QLIK_OAUTH_CLIENTID,
+  redirectUri: import.meta.env.VITE_QLIK_CALLBACK_URL,
   accessTokenStorage: "session",
   authType: "oauth2",
 };
 
 function App() {
-
   return (
     <>
       <QlikEmbedConfig.Provider value={hostConfig}>
